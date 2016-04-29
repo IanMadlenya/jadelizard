bc362bdf56b5633e3de251e78ceddc08567412b9
Brendan Burns <brendanburns@Brendans-MBP.home>
2016-04-28 23:15:24 -0400

    C3 demo graph

---
+++ options/static/options/js/chart.js
@@ -0,0 +1,14 @@
+
+
+
+var d3_chart = c3.generate({
+    data: {
+        x: 'x',
+        columns: [
+            ['x', 30, 50, 100, 230, 300, 310],
+            ['Profit', 30, 200, 100, 400, 150, 250],
+        ]
+    },
+    bindto: "#d3_chart", 
+
+});
