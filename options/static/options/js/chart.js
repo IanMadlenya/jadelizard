f4d46789708f2f4099748e92552fa0d2658d79e5
Brendan Burns <brendanburns@Brendans-MBP.home>
2016-04-29 12:58:55 -0400

    added c3 chart settings

--- options/static/options/js/chart.js
+++ options/static/options/js/chart.js
@@ -2,13 +2,67 @@
 
 
 var d3_chart = c3.generate({
+	
 	data: {
 		x: 'Price',
 		columns: [
 			['Price', 30, 50, 100, 230, 300, 310],
 			['Profit', 30, 200, 100, 400, 150, 250],
-		]
+		],
+
+		type:'area',
+		empty: {
+			label: {
+				text: "Create a Strategy or Load a Template to begin."
+			}
+		},
+		colors: {
+			Profit: '#15e596',
+		},
+		selection: {
+			enabled: true,
+			multiple: false,
+		},
 	},
+
+	axis: {
+		x: {
+			label: {
+				text: "Price of Underlying Security at First Expiration",
+				position: "outer-middle"
+			}
+		},
+		y: {
+			label: {
+				text: "Strategy Profit", 
+				position: "outer-middle",
+			}
+		}
+	},
+
+	grid: {
+		x: {
+			show: true
+		},
+		y: {
+			show: true
+		}
+	},
+
+	legend: {
+		show: false,
+	},
+
+	zoom: {
+		enabled: true,
+		rescaled: true,
+		extent: [1,50]
+	},
+
+	area: {
+		zerobased: false
+	},
+
 	bindto: "#d3_chart", 
 
 });
