e0da20ba155445cf89a9d02216081a6352e7860f
Brendan Burns <brendanburns@Brendans-MBP.home>
2016-04-29 01:50:42 -0400

    start d3 json method

--- options/static/options/js/chart.js
+++ options/static/options/js/chart.js
@@ -2,13 +2,39 @@
 
 
 var d3_chart = c3.generate({
-    data: {
-        x: 'Price',
-        columns: [
-            ['Price', 30, 50, 100, 230, 300, 310],
-            ['Profit', 30, 200, 100, 400, 150, 250],
-        ]
-    },
-    bindto: "#d3_chart", 
+	data: {
+		x: 'Price',
+		columns: [
+			['Price', 30, 50, 100, 230, 300, 310],
+			['Profit', 30, 200, 100, 400, 150, 250],
+		]
+	},
+	bindto: "#d3_chart", 
 
 });
+
+// var d3_chart = c3.generate({
+
+// 	data: {
+
+// 		json: [
+
+// 		],
+		
+// 		keys: {
+// 			x: ''
+// 		}
+
+
+
+// 	}
+
+
+
+
+
+
+
+// 	bindto: "#d3_chart", 
+
+// });
